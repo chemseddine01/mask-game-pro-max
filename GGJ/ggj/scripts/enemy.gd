ac1f631466136c407fd7f2d48e51b16b6aad2c42
chemseddine01 <elmalkichemseddine@gmail.com>
2026-01-31 21:16:30 +0100

    enemy AI

--- GGJ/ggj/scripts/enemy.gd
+++ GGJ/ggj/scripts/enemy.gd
@@ -3,3 +3,8 @@ extends CharacterBody3D
 var dir = randi_range(0,9)
 var speed = 4
 var jump #for no reason :) 
+
+
+func _physics_process(delta: float) -> void:
+	match dir:
+		1: velocity.z = speed
